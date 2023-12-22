9c75085049317d49716c55c19a5bdf53f70b20a2
+collins <+collinskipngetich60@gmail.com>
2023-12-22 17:07:20 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define currentUser
+const currentUser = 'Grace Hopper';
+
+// Create welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Convert welcomeMessage to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
